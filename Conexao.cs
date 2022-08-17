a8bc25894c2d94823dc5c6baede6dcb559916db1
Jean Carlos Horacio <jean.horacio35@gmail.com>
2022-08-17 19:31:44 -0300

    Comentario line 25

--- Conexao.cs
+++ Conexao.cs
@@ -22,6 +22,7 @@ namespace login
             catch(Exception ex)
             {
                 MessageBox.Show(ex.Message);
+              //Comentario Jean
             }
                
         }
